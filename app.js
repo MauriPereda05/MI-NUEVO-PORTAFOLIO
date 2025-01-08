d0551b5f467ddfd768b14fb2e57e70ff294235b8
Mauricio Pereda Ruiz <ruizhydramauri09@gmail.com>
2025-01-08 18:51:42 -0500

    Nuevos ajustes a mi portafolio

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Seleccionamos el botón de hamburguesa y el contenedor de navegación
+// Despliegue de nuestro menu para el menu de hamburguesa
 const burgerButton = document.querySelector('.navbar-burger');
 const navbarCollapse = document.querySelector('.navbar-collapse');
 
@@ -6,3 +6,31 @@ const navbarCollapse = document.querySelector('.navbar-collapse');
 burgerButton.addEventListener('click', () => {
     navbarCollapse.classList.toggle('show');
 });
+
+// Seleccionamos todos los enlaces de la navbar
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Añadimos un evento a cada enlace para la animacion
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        // Eliminamos la clase 'active' de todos los enlaces
+        navLinks.forEach(link => link.classList.remove('active'));
+
+        // Añadimos la clase 'active' al enlace clickeado
+        link.classList.add('active');
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const textElement = document.querySelector('.welcome-text');
+    const container = document.querySelector('.welcome-container');
+    
+    // Detectar el fin de la animación
+    textElement.addEventListener('animationend', () => {
+      container.classList.add('written'); // Rellenar el texto con blanco después de la animación
+    });
+});
+
+
+  
+
